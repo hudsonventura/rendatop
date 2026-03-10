66b6dec344582e288124dab9a7700c72e37a4886
Hudson Ventura <venturadevmaster@gmail.com>
2026-03-09 21:23:40 -0400

    Login with M$ and Google buttons

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -153,6 +153,20 @@ const Login = () => {
                                             "Entrar"
                                         )}
                                     </Button>
+                                    <Button variant="outline" className="w-full cursor-pointer" type="button">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="52 42 88 66">
+                                            <path fill="#4285f4" d="M58 108h14V74L52 59v43c0 3.32 2.69 6 6 6" />
+                                            <path fill="#34a853" d="M120 108h14c3.32 0 6-2.69 6-6V59l-20 15" />
+                                            <path fill="#fbbc04" d="M120 48v26l20-15v-8c0-7.42-8.47-11.65-14.4-7.2" />
+                                            <path fill="#ea4335" d="M72 74V48l24 18 24-18v26L96 92" />
+                                            <path fill="#c5221f" d="M52 51v8l20 15V48l-5.6-4.2c-5.94-4.45-14.4-.22-14.4 7.2" />
+                                        </svg>
+                                        Login com Google / GMail
+                                    </Button>
+                                    <Button variant="outline" className="w-full cursor-pointer" type="button">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 21"><path fill="#f35325" d="M0 0h10v10H0z" /><path fill="#81bc06" d="M11 0h10v10H11z" /><path fill="#05a6f0" d="M0 11h10v10H0z" /><path fill="#ffba08" d="M11 11h10v10H11z" /></svg>
+                                        Entrar com a Microsoft / Outlook
+                                    </Button>
 
                                     {erro && (
                                         <Alert variant="destructive">
