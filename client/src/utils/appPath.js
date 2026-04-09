d2dd4b23c0ddac08239167d3b0e425f2ba1654a1
Hudson Ventura <hudsonventura@outlook.com>
2026-04-09 15:43:34 -0400

    Fixing url problem on client

--- client/src/utils/appPath.js
+++ client/src/utils/appPath.js
@@ -1,4 +1,19 @@
-const ROUTER_BASENAME = (import.meta.env.VITE_ROUTER_BASENAME || "/").replace(/\/+$/, "") || "/";
+function getRouterBasename() {
+    const frontendUrl = (import.meta.env.VITE_FRONTEND_URL || "").trim();
+
+    if (!frontendUrl) {
+        return "/";
+    }
+
+    try {
+        const url = new URL(frontendUrl);
+        return url.pathname.replace(/\/+$/, "") || "/";
+    } catch {
+        return "/";
+    }
+}
+
+const ROUTER_BASENAME = getRouterBasename();
 
 export function appPath(path = "/") {
     if (/^https?:\/\//i.test(path)) {
