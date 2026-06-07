02d06a0ffa41ee9425d4b95d98d71db3c10b8426
Hudson Ventura <hudsonventura@outlook.com>
2026-06-06 22:33:28 -0400

    Fixing links

--- landing/nextjs-version/src/config/api.ts
+++ landing/nextjs-version/src/config/api.ts
@@ -2,5 +2,5 @@ export const API_CONFIG = {
   BASE_URL:
     process.env.NEXT_PUBLIC_BASE_URL_SERVER ||
     process.env.BASE_URL_SERVER ||
-    'http://localhost:5000',
+    'NEXT_PUBLIC_BASE_URL_SERVER and BASE_URL_SERVER are not defined',
 } as const;
